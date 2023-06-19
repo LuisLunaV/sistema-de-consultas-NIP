f3a3f6712ab57ddaae8d115c8b72a6e1caa260b9
luislunav <luis.a.luna.v@outlook.com>
2023-06-18 19:46:47 -0600

    Se crea el archivo config.db.js que realizara la conexion a la BD utilizando sequelize

---
+++ database/config.db.js
@@ -0,0 +1,20 @@
+const { Sequelize } = require('sequelize');
+
+const db = new Sequelize( process.env.NAME_DB, process.env.USER_DB, process.env.PASS_DB,{
+host: process.env.HOST_DB,
+dialect: 'mysql'
+});
+
+const dbConnection = async () => {
+    try {
+        await db.authenticate();
+        console.log('Data base in line')
+    } catch (error) {
+        throw new Error( error );
+    }
+};
+
+module.exports =  {
+    db,
+    dbConnection
+}
