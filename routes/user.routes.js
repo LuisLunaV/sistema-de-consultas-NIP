781eddd6ede9a9794ab9bbf8f6c9b72e0f11eb5c
luislunav <luis.a.luna.v@outlook.com>
2023-06-20 15:36:11 -0600

    Se agregan middlewares que nos ayudaran a validar la informacion de los campos de la tabla usuarios

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -3,9 +3,16 @@ const { check } = require('express-validator');
 
 const { postUser } = require('../controllers/user.controller.js');
 
+const { userExists } = require('../helpers/db-validators.js');
+const { validateProperties } = require('../middlewares/validate-properties.js');
+
 const router = Router();
 
-router.post('/', postUser );
+router.post('/', [
+    check('User_Name').custom( userExists ),
+    check('User_Password', 'El password es obligatorio con mas de 9 caracteres').isLength({ min: 9 }),
+    validateProperties
+], postUser );
 
 
 module.exports = router;
