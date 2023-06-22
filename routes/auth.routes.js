59c5152bdc24f0026972573770e2e5dda5f43ec3
luislunav <luis.a.luna.v@outlook.com>
2023-06-21 22:45:54 -0600

    Creamos el endpoint que nos permita realizar autenticaciones

---
+++ routes/auth.routes.js
@@ -0,0 +1,16 @@
+const { Router } = require('express');
+const { check  } = require('express-validator');
+
+const { validateProperties } = require('../middlewares/validate-properties.js');
+
+const { login } = require('../controllers/auth.controller.js');
+
+const router = Router();
+
+router.post('/login', [
+    check('User_Name', 'El nombre de usuario es obligatorio').not().isEmpty(),
+    check('User_Password', 'El password es obligatorio').not().isEmpty(),
+    validateProperties 
+], login)
+
+module.exports = router;
