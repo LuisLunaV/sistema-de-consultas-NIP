2448083b37447133df525f8e635cff9bbff23c39
luislunav <luis.a.luna.v@outlook.com>
2023-06-24 23:26:54 -0600

    Creamos el controlador de consul, el cual tendra el objeto consult y los metodos que nos ayudaran a la gestion del modelo del mismo nombre

---
+++ controllers/consult.controller.js
@@ -0,0 +1,41 @@
+const { request, response } = require('express');
+
+const { Consult } = require('../model/consult.js');
+
+const getDate = require('../utils/get-current-date.js');
+
+const consults = {
+
+    getConsultID: async( req= request, res = response )=>{},
+
+    postConsult: async( req = request, res = response ) => {
+
+        try {
+            const { Consult_UserID } = req.body;
+            
+            const Consult_Date = getDate();
+
+            const consult = new Consult({ Consult_UserID, Consult_Date }); 
+             
+            await consult.save();
+ 
+            return res.status(200).json({
+               consult,
+            })
+            
+        } catch (error) {
+
+            console.log(error)
+
+            return res.status(500).json({
+                err: error,
+                msg: 'Hubo un error en el servidor'
+            })
+        }
+    },
+
+    putConsult: ()=>{}
+
+};
+
+module.exports = consults;
