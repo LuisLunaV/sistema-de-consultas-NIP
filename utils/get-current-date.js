3a916136935694ea11fed6ccf1c3fd09fb5cfe1a
luislunav <luis.a.luna.v@outlook.com>
2023-06-24 23:28:52 -0600

    Cramos la carpeta utils que contandra las funciones o utilizadas de uso general del proyecto

---
+++ utils/get-current-date.js
@@ -0,0 +1,18 @@
+const getDate =() => {
+
+    const date = new Date();
+           
+    const day   = date.getDate();
+    let month   = date.getMonth()+1;
+    const year  = date.getFullYear();
+
+    if( day   < 10 ) day   = '0' + day;
+    if( month < 10 ) month = '0' + month;
+
+    const currentDay = `${ year }/${ month }/${ day }`;
+
+    return currentDay;
+
+};
+
+module.exports = getDate;
