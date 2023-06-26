b0432838eaf4220eb297b88ad6683e1935211b4a
luislunav <luis.a.luna.v@outlook.com>
2023-06-25 20:30:51 -0600

    Creamos el modelo que controlara la tabla consults_details

---
+++ model/consults_detail.js
@@ -0,0 +1,38 @@
+const { DataTypes } = require('sequelize');
+
+const { db } = require('../database/config.db');
+
+const ConsultsDetail = db.define('Consults_detail', {
+    CD_Id:{
+        type: DataTypes.INTEGER,
+        autoIncrement:true,
+        primaryKey: true
+    },
+    CD_ConsultID:{
+        type: DataTypes.INTEGER
+    },
+    CD_BradID:{
+        type: DataTypes.INTEGER
+    },
+    CD_MethodID:{
+        type: DataTypes.INTEGER
+    },
+    CD_ReferenceNum:{
+        type: DataTypes.STRING,
+        require: [true, 'El numero de referencia es obligatorio']
+    },
+    CD_NIP:{
+        type: DataTypes.CHAR,
+        require: true
+    },
+},{
+    tableName: 'Consults_detail',
+}, {
+    defaultScope: {
+        attributes: { exclude: ['createdAt', 'updatedAt'] }
+    }
+});
+
+module.exports = {
+    ConsultsDetail
+}
