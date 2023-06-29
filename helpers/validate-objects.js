970b3bae95635e34f5e173908f742b225b1c09cc
luislunav <luis.a.luna.v@outlook.com>
2023-06-28 20:47:53 -0600

    Agregamos comentario

--- helpers/validate-objects.js
+++ helpers/validate-objects.js
@@ -1,4 +1,4 @@
-
+//Vlidamos que no falte informacion en ninguno de los campos del objeto (body)
 const validateProperties =( body )=>{  
     return Object.values(body).some( value => value === '' );
  };
