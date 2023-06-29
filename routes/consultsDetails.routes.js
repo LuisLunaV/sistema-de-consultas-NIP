74d0e3ec0e512b0ebe1046ebb74d24b041190d86
luislunav <luis.a.luna.v@outlook.com>
2023-06-28 20:49:52 -0600

    Instanciamos el metodo que ejecuta el procedimiento almacenado en la ruta get

--- routes/consultsDetails.routes.js
+++ routes/consultsDetails.routes.js
@@ -2,9 +2,23 @@ const { Router } = require('express');
 
 const { check  } = require('express-validator');
 
-const { postConsultDetail } = require('../controllers/consults_detail.controller');
+const { validateProperties, validarJWT } = require('../middlewares/index.js');
+const { postConsultDetail, getConsultDetailId, getStoredProcedure } = require('../controllers/consults_detail.controller.js');
 
 const router = Router();
 
-router.post('/', postConsultDetail);
+router.get('/',    getStoredProcedure)
+
+router.get('/:id', getConsultDetailId)
+
+router.post('/', [
+validarJWT,
+check('CD_ConsultID','Evite utilizar letras y signos').matches(/^[0-9]+$/),
+check('CD_BradID','Evite utilizar letras y signos').matches(/^[0-9]+$/),
+check('CD_MethodID','Evite utilizar letras y signos').matches(/^[0-9]+$/),
+check('CD_ReferenceNum','Se debe ingresar un numero de referencia').not().isEmpty(),
+check('CD_NIP','Este no es un nip valido').isInt(),
+validateProperties ],
+postConsultDetail);
+
 module.exports = router;
