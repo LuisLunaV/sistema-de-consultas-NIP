0bdbc3b84a59163b655bac0c5549c279a181d8b7
luislunav <luis.a.luna.v@outlook.com>
2023-07-03 12:04:15 -0600

    Se agrega el middleware check y matches para validar que los datos enviados sean validos

--- routes/consult.routes.js
+++ routes/consult.routes.js
@@ -1,12 +1,12 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
 
-const { postConsult, putConsult } = require('../controllers/consult.controller.js');
+const { postConsult } = require('../controllers/consult.controller.js');
 
 const router = Router();
 
-router.post('/', postConsult );
-router.put('/:id',);
-
+router.post('/',[
+    check('CD_ConsultID','Solo IDs validos').matches(/^[0-9]+$/)
+    ], postConsult );
 
 module.exports = router;
