733a6637e27a736cb6650cbee93419f6cf0db341
luislunav <luis.a.luna.v@outlook.com>
2023-07-04 15:23:58 -0600

    Se remplaza la palabra clave const por let de las variables

--- utils/get-current-date.js
+++ utils/get-current-date.js
@@ -1,10 +1,10 @@
-const getDate =() => {
+const getDates =() => {
 
-    const date = new Date();
+    let date = new Date();
            
-    const day   = date.getDate();
+    let day   = date.getDate();
     let month   = date.getMonth() + 1;
-    const year  = date.getFullYear();
+    let year  = date.getFullYear();
 
     if( day   < 10 ) day   = '0' + day;
     if( month < 10 ) month = '0' + month;
@@ -15,4 +15,6 @@ const getDate =() => {
 
 };
 
-module.exports = getDate;
+module.exports = {
+    getDates
+};
