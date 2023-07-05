e03d64df8dc9381d75b301030a7f6a28f3aa52f7
luislunav <luis.a.luna.v@outlook.com>
2023-07-05 17:31:00 -0600

    Se modifican helpers

--- helpers/db-validators.js
+++ helpers/db-validators.js
@@ -1,20 +1,22 @@
 const { User } = require('../model/user.js');
-
 /**
  * Verifica si ya existe un usuario con el mismo nombre.
- * @param {string} User_Name - Nombre de usuario a verificar.
+ * @param {string} User_NumEmpleado- Nombre de usuario a verificar.
  * @throws {Error} - Error en caso de que el usuario ya exista.
  */
 
-const userExists = async( User_Name = '' )=>{
+const userExists = async( User_NumEmpleado = '' )=>{
+    console.log(User_NumEmpleado)
+
     const userExists = await User.findOne({
-        where: { User_Name }
+        where: { User_NumEmpleado }
     });
 
     if( userExists ){
-        throw new Error( `El usuario ${ User_Name } ya esta siendo utilizado` );
+        throw new Error( `El usuario ${ User_NumEmpleado} ya esta siendo utilizado` );
     }
 };
+
 const userExistById = async( id )=>{
     
     const userExist = await User.findByPk( id );
@@ -22,9 +24,20 @@ const userExistById = async( id )=>{
     if( !userExist ){
         throw new Error(`El usuario con id: ${ id }, no existe`);
     }
+};
+
+const emailExist = async( User_Email = '')=>{
+    console.log(User_Email)
+    const emailExist = await User.findOne({ where: { User_Email }});
+    console.log(emailExist)
+
+    if( emailExist ){
+        throw new Error(`El correo ${ User_Email } ya ha sido registrado`);
+    }
 }
 
 module.exports = {
     userExists,
-    userExistById
+    userExistById,
+    emailExist
 };
