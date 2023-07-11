483f6b06292fa2c20bb10f8a5fb9699d873b4e0c
luislunav <luis.a.luna.v@outlook.com>
2023-07-11 15:40:57 -0600

    Se cambia direccion URl para pruebas en produccion

--- public/assets/js/services/index.js
+++ public/assets/js/services/index.js
@@ -2,7 +2,7 @@
 
 const { url, auth, users, consults, consultsDetails, consultsDate, clients, methods, brands } = {
   // url : "http://localhost:8080",
-  url : "https://sistema-de-consultas-production.up.railway.app/",
+  url : "https://sistema-de-consultas-production.up.railway.app",
   auth: "/auth/login",
   users: "/users",
   consults: "/consults",
