c7bb5d16347af243ec42efb7c984001223ad1afe
luislunav <luis.a.luna.v@outlook.com>
2023-07-12 12:30:05 -0600

    Se cambia direccion URl conexion a BD  para pruebas en produccion

--- database/config.db.js
+++ database/config.db.js
@@ -1,11 +1,11 @@
 const { Sequelize } = require('sequelize');
 
-// const db = new Sequelize( process.env.DB_URL );
+const db = new Sequelize( process.env.DB_URL );
 
-const db = new Sequelize( process.env.NAME_DB, process.env.USER_DB, process.env.PASS_DB,{
-host: process.env.HOST_DB,
-dialect: 'mysql'
-});
+// const db = new Sequelize( process.env.NAME_DB, process.env.USER_DB, process.env.PASS_DB,{
+// host: process.env.HOST_DB,
+// dialect: 'mysql'
+// });
 
 
 /**
--- public/assets/js/services/index.js
+++ public/assets/js/services/index.js
@@ -1,8 +1,8 @@
 // const url = "http://localhost:8080";
 
 const { url, auth, users, consults, consultsDetails, consultsDate, clients, methods, brands } = {
-  url : "http://localhost:8080",
-  // url : "https://sistema-de-consultas-production.up.railway.app",
+  // url : "http://localhost:8080",
+  url : "https://sistema-de-consultas-production.up.railway.app",
   auth: "/auth/login",
   users: "/users",
   consults: "/consults",
