54866a2d97a2865f7f6e49bab043e6c19d74d20f
luislunav <luis.a.luna.v@outlook.com>
2023-07-13 00:22:08 -0600

    Se crea la funcion para obtener la hora de las consultas y darles formato

--- public/assets/js/utils/index.js
+++ public/assets/js/utils/index.js
@@ -3,10 +3,12 @@ import { limpiarModalProductos } from './limpiar-modal.js';
 import { limpiarTablaBody } from './limpiar-tabla-usuarios.js';
 import { fechaActual } from './fecha-actual.js';
 import { obtenerToken } from './obtener-token.js';
+import { horaDeConsulta } from './hora-de-consulta.js';
 export{
     mostrarModalConsulta,
     limpiarModalProductos,
     limpiarTablaBody,
     fechaActual,
-    obtenerToken
+    obtenerToken,
+    horaDeConsulta
 }
