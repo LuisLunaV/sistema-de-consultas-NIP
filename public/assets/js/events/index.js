597d93231a87431209259598d7691624f782f5a2
luislunav <luis.a.luna.v@outlook.com>
2023-07-18 22:28:50 -0600

    Creamos el evento de para obtencion de datos en registro

--- public/assets/js/events/index.js
+++ public/assets/js/events/index.js
@@ -1,7 +1,9 @@
-import{ consultarNip }from'./consultar-nip.js';
-import{ cerrarSesion }from'./cerrar-sesion.js';
+import { consultarNip } from'./consultar-nip.js';
+import { cerrarSesion } from'./cerrar-sesion.js';
+import { obtenerDatosDelEmpleado } from'./obtener-datos-empleado.js';
 
 export{
     consultarNip,
-    cerrarSesion
+    cerrarSesion,
+    obtenerDatosDelEmpleado
 }
