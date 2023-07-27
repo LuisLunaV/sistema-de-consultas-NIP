00b30735e37de4d92fc3fa16dffc50e29fe68690
luislunav <luis.a.luna.v@outlook.com>
2023-07-27 16:42:31 -0600

    Se agregan comentarios al codigo de loader.js

--- public/assets/js/utils/loader.js
+++ public/assets/js/utils/loader.js
@@ -4,11 +4,13 @@ export const componentesCargados = () => {
   window.onload = () => {
   
     const loader = document.querySelector(".loader");
+    
+    //Agregamos la clase ocultar que agrega opacity: 0
     loader.classList.add("ocultar");
-  
+
+    //Agregamos display = "none" para que el elemento con la animacion desaparesca y no consuma recursos
     setTimeout(() => {
       loader.style.display = "none";
-      loader.classList.remove("hide");
     }, 300);
 
   };
