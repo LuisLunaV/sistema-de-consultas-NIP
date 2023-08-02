53866fa304eff7b3884eb44c51a63cbe5c9cac63
luislunav <luis.a.luna.v@outlook.com>
2023-08-02 14:45:29 -0600

    Se agrega formato al codigo

--- utils/get-current-date.js
+++ utils/get-current-date.js
@@ -3,7 +3,7 @@ const getDates =() => {
     let date = new Date();
            
     let day   = date.getDate();
-    let month   = date.getMonth() + 1;
+    let month = date.getMonth() + 1;
     let year  = date.getFullYear();
 
     if( day   < 10 ) day   = '0' + day;
