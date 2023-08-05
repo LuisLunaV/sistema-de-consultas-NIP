8c13d53aab1c5825383800159e2390b0d188248f
luislunav <luis.a.luna.v@outlook.com>
2023-08-05 13:58:58 -0600

    Creamos un nuevo router para la gestion del servicio

---
+++ routes/consultPhoneAndMembership.routes.js
@@ -0,0 +1,13 @@
+const { Router } = require('express');
+const { validateProperties, validarJWT } = require('../middlewares/index.js');
+
+const { postPhoneAndMembership } = require('../controllers/consult_phone_membership.js');
+
+const router = Router();
+
+router.post('/',[
+    validarJWT,
+    validateProperties
+], postPhoneAndMembership)
+
+module.exports = router;
