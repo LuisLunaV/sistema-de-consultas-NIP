d305aef118208124232f18c0bc95e12591c5c783
luislunav <luis.a.luna.v@outlook.com>
2023-08-08 14:15:08 -0600

    Filtramos los metodos para evitar que se imprima la opcion de combinados

--- src/js/utils/imprimir-metodos.js
+++ src/js/utils/imprimir-metodos.js
@@ -3,5 +3,12 @@ import { htmlMetodos } from '../components/index.js';
 
 export const imprimirMetodos = async()=>{
    const metodos = await getMetodos();
-   metodos.forEach( htmlMetodos ); 
+
+   //Filtramos los metodos para evitar que se imprima la opcion de combinados.
+  const imprimirMetodos = metodos.filter(metodo =>{
+      if(metodo.Method_Id != 4) return metodo; 
+   })
+
+   imprimirMetodos.forEach( htmlMetodos );
+
 }
