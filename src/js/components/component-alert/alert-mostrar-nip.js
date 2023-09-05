cbcd09715aa3a8d01d2777db6eeb9d66b3515678
luislunav <luis.a.luna.v@outlook.com>
2023-09-05 12:33:38 -0600

    Agregamos la funcion para realizar consulta a la api sms y actualizar el estado sms de la consulta

--- src/js/components/component-alert/alert-mostrar-nip.js
+++ src/js/components/component-alert/alert-mostrar-nip.js
@@ -1,8 +1,9 @@
-import { formarDataDeSmS } from '../../helper/obtener-datos-sms.js';
 import { smsServices } from '../../services/sms-services.js';
+import { patchConsultDetails } from '../../services/api-post.js';
+import { formarDataDeSmS } from '../../helper/obtener-datos-sms.js';
 import { statusSmS } from './alert-status-sms.js';
 
-export const mostrarNip = ( { NIP, TELEFONO } )=>{
+export const mostrarNip = ( { NIP, TELEFONO, result } )=>{
 
         Swal.fire({
           title: `¿Deseas enviar el NIP al numero ${TELEFONO} del cliente?`,
@@ -31,12 +32,21 @@ export const mostrarNip = ( { NIP, TELEFONO } )=>{
                 const dataSmS = formarDataDeSmS( NIP, TELEFONO )
                 
                 //Enviamos el nip y el numero tel. para realizar el envio del sms
-                const { success, code } = await smsServices( dataSmS )
+                const { id, success, code } = await smsServices( dataSmS )
                 .catch((err) =>{
                   console.log(`El erro es: ${err}`)
                 });
 
-                
+                //Creamos la carga para realizar la auditoria SMS y acutalizar la consulta
+                const payload = {
+                  ...result,
+                  messageid: id
+                }
+
+
+              const resp = await patchConsultDetails( payload )
+                console.log(resp)
+
                 //Devuelve la respuesta de entrega de sms.
                 statusSmS( success, code );
                
