aa9e863386a740083b6cd24b4082ac3d6d7969ed
luislunav <luis.a.luna.v@outlook.com>
2023-09-05 12:28:09 -0600

    Se configura el algoritmo que se encargara de realizar la auditoria a la api sms

--- helpers/init-polling.js
+++ helpers/init-polling.js
@@ -1,27 +1,21 @@
-const { postConsultDetail } = require("../controllers/consults_detail.controller.js");
-const { smsStatus }         = require("../service/sms-services.js");
+// const { postConsultDetail } = require("../controllers/consults_detail.controller.js");
+const { smsStatus } = require("../service/sms-services.js");
 
-const initigPolling = async( payload, info )=>{
+const initigPolling = async( payload )=>{
     
     // Iniciar el polling para verificar la entrega del SMS
     const interval = 10000; // Intervalo de polling en milisegundos (10 segundos)
-    let delivered = false;
+    let   succes   = false;
 
-    while ( !delivered ) {
+    while ( !succes ) {
       // Obtener el estado del SMS
       const resp = await smsStatus( payload );
       console.log(resp);
-      if (resp.code === 3) {
+      if (resp.code != 6) {
         // SMS entregado, continuar con el proceso
-        delivered = true;
-
-        // Creamos la data que se guardara en la BD.
-        const data = {
-          ...info,
-          CD_Status_SMS: resp.code,
-        };
-
-        await postConsultDetail({ ...data });
+        succes = true;
+        
+        return resp;
       } else {
         // Esperar el próximo ciclo de polling
         await new Promise((resolve) => setTimeout(resolve, interval));
